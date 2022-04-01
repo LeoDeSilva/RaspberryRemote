f05d7c6964387b62e64a0ad610963cdbfa1a8d24
Leo De Silva <leo.desilvafamily@gmail.com>
2022-04-01 12:55:01 +0100

    sent post request with url, will be automatically searched

---
+++ go.mod
@@ -0,0 +1,8 @@
+module RemotePI
+
+go 1.17
+
+require (
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
+	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,40 @@ package main
 
 import (
   "fmt"
+  "net/http"
+  "log"
+  "github.com/pkg/browser"
 )
 
+const PORT = ":10000"
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("Hello World")
+}
+
+func linkPage(w http.ResponseWriter, r *http.Request) {
+  switch r.Method {
+  case "GET":
+    fmt.Println("Expected POST Request at /link")
+  case "POST":
+    if err := r.ParseForm(); err != nil {
+      fmt.Printf("ParseForm() err: %v", err);
+      return
+    }
+
+    url := r.FormValue("url")
+    fmt.Println("URL",url)
+    browser.OpenURL(url)
+  }
+}   
+
+func handleRequests() {
+  http.HandleFunc("/", homePage) 
+  http.HandleFunc("/link", linkPage) 
+  log.Fatal(http.ListenAndServe(PORT, nil))
+}
+
 func main() {
-  fmt.Println("Hello World");
+  fmt.Printf("RemotePI - Server open on port: '%s'\n", PORT)
+  handleRequests()
 }
